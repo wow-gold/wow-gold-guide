f15a5816c529fe5f3e7ed801437894436c1ce992
John Koerner <johnkoern@gmail.com>
2025-01-23 23:58:18 -0500

    Fixing some ollama weirdness on conversion.  Adding wowhead zone support.

---
+++ src/components/WowHeadZone.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function WowHeadZone({children, id}) {
+    var url = "https://www.wowhead.com/zone=" + id;
+    var item = "zone=" + id;
+  return (
+    
+<a href={url}  data-wowhead={item} data-game="wow" data-type="item" target="_blank"  data-wh-rename-link="true"></a>
+
+  );
+}
--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -2,6 +2,7 @@ import React from 'react';
 // Import the original mapper
 import MDXComponents from '@theme-original/MDXComponents';
 import WowHeadItem from '@site/src/components/WowHeadItem';
+import WowHeadZone from '@site/src/components/WowHeadZone';
 
 export default {
   // Re-use the default mapping
@@ -9,4 +10,5 @@ export default {
   // Map the "<Highlight>" tag to our Highlight component
   // `Highlight` will receive all props that were passed to `<Highlight>` in MDX
   WowHeadItem,
+  WowHeadZone
 };
